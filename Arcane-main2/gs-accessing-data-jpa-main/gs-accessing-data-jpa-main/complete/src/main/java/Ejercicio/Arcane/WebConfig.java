7f88e666fa69d6c10b2509cfcc26046e55743e92
Vanessa <vtorres2004.1211@gmail.com>
2025-04-28 23:41:06 -0500

    arreglos

--- Arcane-main2/gs-accessing-data-jpa-main/gs-accessing-data-jpa-main/complete/src/main/java/Ejercicio/Arcane/WebConfig.java
+++ Arcane-main2/gs-accessing-data-jpa-main/gs-accessing-data-jpa-main/complete/src/main/java/Ejercicio/Arcane/WebConfig.java
@@ -1,20 +1,22 @@
 package Ejercicio.Arcane;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-public class WebConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-            }
-        };
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // Esto hace que al entrar a localhost:8080/ muestre tu personajes.html
+        registry.addViewController("/").setViewName("forward:/Arcane.html");
     }
 }
+
